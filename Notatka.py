e60bcbd08e5663ab3140c9165d7bcbe4bb089a77
OlekCydejko <olekcydejko98@gmail.com>
2024-03-17 12:09:10 +0100

    Propozycja ze szczegółowym opisem (Wynajem samochodów)

--- Notatka.py
+++ Notatka.py
@@ -5,7 +5,7 @@ M - Michał
 O - Olek
 
 MOJpython
-
+sadasadaasdasdasadasd
 
 Pomysły na aplikacje
 - komunikator
@@ -16,3 +16,48 @@ Motto
 "Razem możemy więcej!"
 
 "Test"
+##############################################################################################################################################
+
+Nazwa zespołu: DriveEase
+
+Motto: "Nasza droga, Twój komfort."
+
+Projekt logo: Link do logo
+
+
+O logo:
+
+Logo przedstawia dynamiczną, stylizowaną literę "D", która jednocześnie przypomina kształt klucza samochodowego, symbolizując dostęp i wygodę.
+Kolorystyka w odcieniach niebieskiego i zielonego symbolizuje spokój, zaufanie i środowisko, co podkreśla nasze zaangażowanie w ekologiczny aspekt wynajmu samochodów.
+Czyste, proste linie i minimalistyczny design dodają profesjonalizmu i nowoczesności.
+
+
+
+# Projekt Aplikacji Wynajmu Samochodów
+
+"""## Cel:
+Nasz projekt ma na celu stworzenie kompleksowej aplikacji do wynajmu samochodów, która zapewni użytkownikom łatwy dostęp do szerokiej gamy pojazdów oraz ułatwi proces rezerwacji i zarządzania wypożyczeniami.
+
+## Zakres:
+- Utworzenie platformy internetowej oraz aplikacji mobilnej dla systemów Android i iOS.
+- Integracja z systemami płatności online, umożliwiając płatności kartą kredytową i innymi popularnymi metodami płatności.
+- Implementacja systemu rezerwacji, umożliwiającego użytkownikom wybór daty, czasu i miejsca odbioru oraz zwrotu pojazdu.
+- Stworzenie panelu administracyjnego dla pracowników firmy do zarządzania flotą samochodów, rezerwacjami oraz klientami.
+- Zapewnienie możliwości oceniania i recenzowania zarówno pojazdów, jak i doświadczeń użytkowników.
+
+## Funkcjonalności:
+- Rejestracja i logowanie użytkowników.
+- Przeglądanie dostępnych pojazdów z filtrowaniem według preferencji.
+- Wybór daty, czasu i miejsca odbioru oraz zwrotu pojazdu.
+- Realizacja płatności online.
+- Generowanie potwierdzeń rezerwacji i faktur.
+- Historia rezerwacji i możliwość anulowania rezerwacji.
+- Ocena i recenzowanie pojazdów i doświadczeń użytkowników.
+- Zarządzanie profilem użytkownika.
+- Zarządzanie flotą samochodów i rezerwacjami przez panel administracyjny.
+
+## Oczekiwane rezultaty:
+- Stworzenie intuicyjnej, łatwej w obsłudze aplikacji, która zapewni użytkownikom wygodne doświadczenie w wynajmie samochodów.
+- Zwiększenie dostępności i wygody procesu wynajmu samochodów dla klientów.
+- Zwiększenie efektywności w zarządzaniu flotą samochodów i rezerwacjami przez firmę.
+- Zadowolenie klientów oraz budowanie pozytywnego wizerunku marki poprzez wysoką jakość usług i obsługi."""
