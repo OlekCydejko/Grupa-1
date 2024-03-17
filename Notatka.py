69118b911d944949fce0c736756d776973c4188a
OlekCydejko <olekcydejko98@gmail.com>
2024-03-17 12:22:21 +0100

    update

--- Notatka.py
+++ Notatka.py
@@ -1,35 +1,35 @@
-Nazwa:
-JMO
-J - Jan
-M - Michał
-O - Olek
+# Nazwa:
+# JMO
+# J - Jan
+# M - Michał
+# O - Olek
 
-MOJpython
-sadasadaasdasdasadasd
+# MOJpython
+# sadasadaasdasdasadasd
 
-Pomysły na aplikacje
-- komunikator
-- wypożyczalnia aut 
-- system lojalnoiściowy
+# Pomysły na aplikacje
+# - komunikator
+# - wypożyczalnia aut 
+# - system lojalnoiściowy
 
-Motto 
-"Razem możemy więcej!"
+# Motto 
+# "Razem możemy więcej!"
 
-"Test"
+# "Test"
 ##############################################################################################################################################
 
-Nazwa zespołu: DriveEase
+# Nazwa zespołu: DriveEase
 
-Motto: "Nasza droga, Twój komfort."
+# Motto: "Nasza droga, Twój komfort."
 
-Projekt logo: Link do logo
+# Projekt logo: Link do logo
 
 
-O logo:
+# O logo:
 
-Logo przedstawia dynamiczną, stylizowaną literę "D", która jednocześnie przypomina kształt klucza samochodowego, symbolizując dostęp i wygodę.
-Kolorystyka w odcieniach niebieskiego i zielonego symbolizuje spokój, zaufanie i środowisko, co podkreśla nasze zaangażowanie w ekologiczny aspekt wynajmu samochodów.
-Czyste, proste linie i minimalistyczny design dodają profesjonalizmu i nowoczesności.
+# Logo przedstawia dynamiczną, stylizowaną literę "D", która jednocześnie przypomina kształt klucza samochodowego, symbolizując dostęp i wygodę.
+# Kolorystyka w odcieniach niebieskiego i zielonego symbolizuje spokój, zaufanie i środowisko, co podkreśla nasze zaangażowanie w ekologiczny aspekt wynajmu samochodów.
+# Czyste, proste linie i minimalistyczny design dodają profesjonalizmu i nowoczesności.
 
 
 
@@ -61,3 +61,28 @@ Nasz projekt ma na celu stworzenie kompleksowej aplikacji do wynajmu samochodów
 - Zwiększenie dostępności i wygody procesu wynajmu samochodów dla klientów.
 - Zwiększenie efektywności w zarządzaniu flotą samochodów i rezerwacjami przez firmę.
 - Zadowolenie klientów oraz budowanie pozytywnego wizerunku marki poprzez wysoką jakość usług i obsługi."""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Przykładowa biblioteka samochodów
+cars = [
+    {'brand': 'Toyota', 'model': 'Corolla', 'year': 2020, 'price': 150},
+    {'brand': 'Honda', 'model': 'Civic', 'year': 2019, 'price': 160},
+    {'brand': 'Ford', 'model': 'Focus', 'year': 2018, 'price': 140},
+    {'brand': 'Volkswagen', 'model': 'Golf', 'year': 2021, 'price': 170},
+    {'brand': 'BMW', 'model': '3 Series', 'year': 2017, 'price': 200},
+    {'brand': 'Audi', 'model': 'A4', 'year': 2019, 'price': 220},
+    {'brand': 'Mercedes-Benz', 'model': 'C-Class', 'year': 2020, 'price': 250},
+    {'brand': 'Hyundai', 'model': 'Elantra', 'year': 2018, 'price': 130},
+    {'brand': 'Kia', 'model': 'Forte', 'year': 2019, 'price': 135},
+    {'brand': 'Nissan', 'model': 'Sentra', 'year': 2020, 'price': 140}
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', cars=cars)
+
+if __name__ == '__main__':
+    app.run(debug=True)
